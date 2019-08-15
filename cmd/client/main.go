cef5f21cc887862da6d7399dfb30debe35aaa17c
Tushar Bankar <tabankar@gmail.com>
2019-08-15 21:15:07 +0530

    Logger code added

---
+++ cmd/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gomessenger/pkg/proto"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:8443", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	client := proto.NewMessengerServiceClient(conn)
+	cuser, err := client.CreateUser(context.Background(), &proto.CreateUserInput{Username: "tushar", Name: " Tushar Bankar", Email: "tabankar@gmail.com"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cuser)
+
+}
